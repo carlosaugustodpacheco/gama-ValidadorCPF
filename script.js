a5588b9b325dbedde99a5967ea7dd6212e9cfcdf
Carlos Augusto D. Pacheco <carlos.pacheco@aluno.ifsp.edu.br>
2022-05-10 15:27:04 -0300

    Desafio III completo

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 console.log('Javascript carregado')
 
 function validaCPF(cpf) {
+    //  console.log( "o cpf tem a quantidade de" + cpf.length);
+    if (cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring (9)
+        // console.log("numero do cpf" + numeros)
+        // console.log("digitos do cpf" + digitos)
+
+        var soma = 0
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        console.log(soma);
+        return true;
+    }
 
 }
 function validacao () {
     console.log('Iniciando validação CPF');
+
    var cpf = document.getElementById('cpf_digitado').value
-   console.log(cpf);
 
    var resultadoValidadacao = validaCPF(cpf);
 
