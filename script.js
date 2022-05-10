440fcf26974b9522bbfb49b0dd4cbe7e209983bc
Carlos Augusto D. Pacheco <carlos.pacheco@aluno.ifsp.edu.br>
2022-05-10 15:59:26 -0300

    Versão final funcional

--- script.js
+++ script.js
@@ -25,12 +25,26 @@ function validaCPF(cpf) {
             return false
         }
         
+        soma = 0;
+        numeros = cpf.substring (0, 10);
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+        //Validacao do segundo digito
+        if(resultado != digitos.charAt(1)) {
+            return false;
+        }
+
         return true;
     }
 
 }
 function validacao () {
     console.log('Iniciando validação CPF');
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 
    var cpf = document.getElementById('cpf_digitado').value
 
